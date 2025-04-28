bd62648287bf9a01243436fb4501515e7ae094fa
DavidGuerrero0803 <mario.guerrero.marquez@uabc.edu.mx>
2025-04-28 09:18:20 -0700

    agregados los métodos modoSeleccionado() y mostrarReglas()

--- src/ConsoleUI.java
+++ src/ConsoleUI.java
@@ -11,4 +11,24 @@ public class ConsoleUI {
         }
         return jugadores;
     }
+
+    public static int modoSeleccionado() {
+        System.out.print("Selecciona el modo de juego (1: Regular, 2: Experto): ");
+        return scanner.nextInt();
+    }
+
+    public static void mostrarReglas(int mode) {
+        if (mode == 1) {
+            System.out.println("\nREGLAS DEL MODO REGULAR:");
+            System.out.println("* Sin acentos ni Ñ");
+            System.out.println("* 5 pts por vocal, 3 pts por consonante");
+            System.out.println("* -5 pts por palabra inválida");
+        } else {
+            System.out.println("\nREGLAS DEL MODO EXPERTO:");
+            System.out.println("* Acentos obligatorios donde corresponda");
+            System.out.println("* Incluye letra Ñ (20% de probabilidad)");
+            System.out.println("* -10 pts por palabra inválida");
+        }
+    }
+
 }
