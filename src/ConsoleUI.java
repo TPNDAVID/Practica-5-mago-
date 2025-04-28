ff73716646a8baf35889e8270550133c584473f8
DavidGuerrero0803 <mario.guerrero.marquez@uabc.edu.mx>
2025-04-28 09:17:19 -0700

    avance ConsoleUI

--- src/ConsoleUI.java
+++ src/ConsoleUI.java
@@ -1,2 +1,14 @@
+import java.util.*;
+
 public class ConsoleUI {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static List<String> getJugadores(int cantidad) {
+        List<String> jugadores = new ArrayList<>();
+        for (int i = 1; i <= cantidad; i++) {
+            System.out.print("Nombre del jugador " + i + ": ");
+            jugadores.add(scanner.nextLine().trim());
+        }
+        return jugadores;
+    }
 }
